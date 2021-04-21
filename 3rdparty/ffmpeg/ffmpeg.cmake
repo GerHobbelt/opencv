3047691c4c3a8be04323501de53a5a7990e48ca6
Hermeson Barbosa <barbosa@deconve.com.br>
2021-04-21 18:01:59 -0300

    update ffmpeg version

--- 3rdparty/ffmpeg/ffmpeg.cmake
+++ 3rdparty/ffmpeg/ffmpeg.cmake
@@ -1,8 +1,8 @@
-# Binaries branch name: ffmpeg/master_20210303
-# Binaries were created for OpenCV: 7ac6abe02a33bef445a5b77214ad31964e2c5cc1
-ocv_update(FFMPEG_BINARIES_COMMIT "629590c3ba09fb0c8eaa9ab858ff13d3a84ca1aa")
-ocv_update(FFMPEG_FILE_HASH_BIN32 "638065d5a0dab8a828879942375dcac4")
-ocv_update(FFMPEG_FILE_HASH_BIN64 "7f10ae2e6a080ba3714f7a38ee03ae15")
+# Binaries branch name: ffmpeg/master_20210421
+# Binaries were created for OpenCV: 1f0a4db31e9fd1126952b5d2b9441fb8454063a9
+ocv_update(FFMPEG_BINARIES_COMMIT "8f6893d0a71c782ff851206f8db8ea5ad21732c9")
+ocv_update(FFMPEG_FILE_HASH_BIN32 "37265463aaa97185b8cc9298649ad633")
+ocv_update(FFMPEG_FILE_HASH_BIN64 "8ac4c14aad1bebc35ce9b63dd228d0e7")
 ocv_update(FFMPEG_FILE_HASH_CMAKE "f8e65dbe4a3b4eedc0d2997e07c3f3fd")
 
 function(download_win_ffmpeg script_var)
