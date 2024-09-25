b2c01392ec4779abf5e0f804a4e8a4db24bfeb0f
Marcos Cesar Gritti <marcos@goodones.app>
2024-09-25 12:57:58 -0300

    feat: create build script for iOS ignoring unused modules

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Build opencv.xcframework for iOS
+
+OUTPUT_DIR=../FaceSDK/FaceSDK/Lib
+
+python platforms/apple/build_xcframework.py \
+    --out ${OUTPUT_DIR} \
+    --without=video \
+    --without=videoio \
+    --without=highgui \
+    --without=calib3d \
+    --without=features2d \
+    --without=objdetect \
+    --without=ml \
+    --without=flann \
+    --without=photo \
+    --without=stitching \
+    --without=gapi \
+    --iphoneos_archs=arm64 \
+    --iphonesimulator_archs=arm64 \
+    --iphoneos_deployment_target=15 \
+    --disable-bitcode \
+    --build_only_specified_archs
+
+
+
+
